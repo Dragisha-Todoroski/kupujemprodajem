c84e694a5468fe3b830ac01aae9db583f3eb7ea1
Dragisha-Todoroski <dade99mk@gmail.com>
2025-08-16 22:34:09 +0200

    Created UserRole enum to enforce specific user types. Changed  property in  method and  method.

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Str;
+use App\Enums\UserRole;
 
 class User extends Authenticatable
 {
@@ -33,11 +34,13 @@ class User extends Authenticatable
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'role' => UserRole::class,
         ];
     }
 
-    public function isAdmin() {
-        return $this->role === 'admin';
+    public function isAdmin(): bool
+    {
+        return $this->role === UserRole::ADMIN;
     }
 
     protected static function boot() {
