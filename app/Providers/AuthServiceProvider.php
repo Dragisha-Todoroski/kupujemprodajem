93794a94232c962c40f2e3599d3811bcbcb5d5c7
Dragisha-Todoroski <dade99mk@gmail.com>
2025-08-16 04:37:15 +0200

    Created AuthServiceProvider and mapped Category model to CategoryPolicy, before registering policies.

---
+++ app/Providers/AuthServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Gate;
+use App\Models\Category;
+use App\Policies\CategoryPolicy;
+
+class AuthServiceProvider extends ServiceProvider
+{
+    /**
+     * The policy mappings for the application.
+     */
+    protected $policies = [
+        // Map category to its policy
+        Category::class => CategoryPolicy::class,
+    ];
+
+    /**
+     * Register any authentication / authorization services.
+     */
+    public function boot(): void
+    {
+        $this->registerPolicies();
+    }
+}
